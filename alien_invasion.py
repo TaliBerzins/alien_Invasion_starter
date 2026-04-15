0861a19b1a8ead35ba2b3db963d2d170e1340001
TaliBerzins <tali.berzins@outlook.com>
2026-04-15 15:28:25 -0400

    P2 - Created a single row of stationary aliens for alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,8 @@ import pygame
 from GameSettings import Settings
 from ship import Ship
 from arsenal import Arsenal
-from alien import Alien
+# from alien import Alien
+from alien_fleet import AlienFleet
 
 class AlienInvasion:
     def __init__(self):
@@ -25,14 +26,15 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.7)
 
         self.ship = Ship(self,Arsenal(self))
-        self.alien = Alien(self, 10, 10)
+        self.alien_fleet = AlienFleet(self)
+        self.alien_fleet.create_fleet() 
 
     def run_game(self):
         #Game Loop
         while self.running:
             self._check_events()
             self.ship.update()
-            self.alien.update()
+            #self.alien.update()
 
 
             self._update_screen()
@@ -41,7 +43,7 @@ class AlienInvasion:
     def _update_screen(self):
         self.screen.blit(self.bg,(0,0))
         self.ship.draw()
-        self.alien.draw_alien()
+        self.alien_fleet.draw()
         pygame.display.flip()
 
     def _check_events(self):
